51804b615bf875e89d11985c5b889275b20ac662
tetti90 <slobodyanoleg5@gmail.com>
2017-12-02 01:22:08 +0200

    add stiker hit

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,8 @@
-<script>
-	$(".menu__btn").on("click", function(){
-		 $(this).toggleClass("menu__btn_active");
-		 $(".menu__list").slideToggle(500)
+$(".menu__btn").on("click", function(){
+	$(this).toggleClass("menu__btn_active");
+	$(".menu__list").slideToggle(500)
+});
 
-	});
-	$(".btn-search").on("click", function(){
-		 $(".menu__input").animate({width: "toggle"});
-	});
-</script>
+$(".btn-search").on("click", function(){
+	$(".menu__input").animate({width: "toggle"});
+});
