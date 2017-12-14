a6677e3d85ed254cc047a91d5f5c0fdd8fdf6fb5
olsydor <olsydor@gmail.com>
2017-12-14 19:28:38 +0200

    add lightGallery and left col

--- dev/js/main.js
+++ dev/js/main.js
@@ -122,3 +122,20 @@ $(document).ready(function() {
         onBeforePrevSlide: function (el) {}
     });
 });
+  $(document).ready(function() {
+    $('.imageGallery').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        thumbItem:9,
+        controls: false,
+        slideMargin:0,
+        enableDrag: false,
+        currentPagerPosition:'left',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '.imageGallery .lslide'
+            });
+        }   
+    });  
+  });
